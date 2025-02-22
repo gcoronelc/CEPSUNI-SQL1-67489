2e35fe5268fb193151ca4cb029392353ee933d68
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2025-02-21 21:52:00 -0500

    Clase 2

---
+++ Clase02-21FEB2025/TAREA.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE ALFA; 
+GO 
+
+USE ALFA; 
+GO
+
+
+CREATE TABLE dbo.ALUMNO ( 
+	alu_id INT NOT NULL , 
+	alu_nombre varchar(100) NOT NULL , 
+	alu_direccion varchar(100) NOT NULL , 
+	alu_telefono varchar(20) NULL , 
+	alu_email varchar(50) NULL 
+); 
+GO
+
+
+
+
---
+++ Clase02-21FEB2025/Work.sql
@@ -0,0 +1,33 @@
+
+CREATE DATABASE BD_PRUEBA;
+GO
+
+USE BD_PRUEBA;
+GO
+
+CREATE TABLE Esquema2.DATOS1(
+  MENSAJE VARCHAR(1000)
+) ON Gustavo;
+GO
+
+
+-- Base de datos: 
+-- Esquema: 
+-- Grupo de archivos: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
